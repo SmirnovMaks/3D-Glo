bc7a9c16d991657df5868446c853e2085b4d6b12
Maksim Smirnov <perforator01@gmail.com>
2022-06-30 20:48:58 +0300

    плавные скролы

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,6 +3,15 @@ const menu = () => {
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
     const menuItems = menu.querySelectorAll('ul>li>a');
+    const nextSlideBtn = document.querySelector('a[href="#service-block"]');
+
+    nextSlideBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelector("#service-block").scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
 
     const handleMenu = () => {
         menu.classList.toggle('active-menu');
@@ -11,6 +20,17 @@ const menu = () => {
     menuBtn.addEventListener('click', handleMenu);
     closeBtn.addEventListener('click', handleMenu);
 
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener('click', (event) => {
+            handleMenu();
+            event.preventDefault();
+            const id = menuItem.getAttribute('href');
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
 };
 export default menu;
