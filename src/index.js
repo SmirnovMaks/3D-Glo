20bd36f6ecde81c18dfa413a8714e4ae54b098da
Maksim Smirnov <perforator01@gmail.com>
2022-07-02 01:00:43 +0300

    валидация

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import formValidator from "./modules/form-validator";
 
 timer('3 july 2022');
 menu();
 modal();
+formValidator();
---
+++ src/modules/form-validator.js
@@ -0,0 +1,34 @@
+const formValidator = () => {
+    const calcItem = document.querySelectorAll('input.calc-item');
+    const inputTel = document.querySelectorAll('input[type=tel]');
+    const inputEmail = document.querySelectorAll('input[type=email]');
+    const formName = document.querySelectorAll('#form2-name, .mess, .form-name');
+
+    console.log(formName);
+
+    formName.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-Я- ]/, "");
+        });
+    });
+
+    inputTel.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^-)(\d]/, "");
+        });
+    });
+
+    inputEmail.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^ @ - _ .!~*'\w]/, "");
+        });
+    });
+
+    calcItem.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/\D+/, "");
+        });
+    });
+};
+
+export default formValidator;
