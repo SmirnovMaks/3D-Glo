5e5484cead1f618116d5ab1e2abe8b8480fa37db
Maksim Smirnov <perforator01@gmail.com>
2022-07-04 04:22:19 +0300

    калькулятор

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal";
 import formValidator from "./modules/form-validator";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 timer('7 july 2022');
 menu();
@@ -11,3 +12,4 @@ modal();
 formValidator();
 tabs();
 slider();
+calc(100);
